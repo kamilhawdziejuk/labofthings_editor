7b83e6b1ef8295924d23f3cb5e2b0611781a4e11
KamilHawdziejuk <kamil.hawdziejuk@gmail.com>
2014-06-08 15:45:08 +0200

    Distance sensor next phase

---
+++ Gadgeteer/DistanceSensor/DistanceSensor/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections;
+using System.Threading;
+using Microsoft.SPOT;
+using Microsoft.SPOT.Presentation;
+using Microsoft.SPOT.Presentation.Controls;
+using Microsoft.SPOT.Presentation.Media;
+using Microsoft.SPOT.Presentation.Shapes;
+using Microsoft.SPOT.Touch;
+
+using Gadgeteer.Networking;
+using GT = Gadgeteer;
+using GTM = Gadgeteer.Modules;
+using Gadgeteer.Modules.GHIElectronics;
+
+namespace DistanceSensor
+{
+    public partial class Program
+    {
+        HomeOSGadgeteer.HomeOSGadgeteerDevice hgd;
+        TimeSpan RemoteControlLedEndTime = TimeSpan.Zero;
+        const int CheckPeriod = 5000;
+
+        // This method is run when the mainboard is powered up or reset.   
+        void ProgramStarted()
+        {
+            hgd = new HomeOSGadgeteer.HomeOSGadgeteerDevice("MicrosoftResearch", "DistanceSensor", "abcdefgh", wifi,
+                null, null, /*usbSerial.SerialLine.PortName*/null, null, null, () => { return GT.Timer.GetMachineTime() < RemoteControlLedEndTime; }, true);
+
+            GT.Timer timer = new GT.Timer(CheckPeriod);
+            timer.Tick += Timer_Tick;
+            timer.Start();
+
+            hgd.SetupWebEvent("distance").WebEventReceived += WebEventReceived;
+            Debug.Print("Program Started");
+        }
+
+        void Timer_Tick(GT.Timer timer)
+        {
+            Debug.Print(this.response);
+        }
+
+        void WebEventReceived(string path,
+            HomeOSGadgeteer.Networking.WebServer.HttpMethod method,
+            HomeOSGadgeteer.Networking.Responder responder)
+        {
+            Debug.Print("Distance web event from " + responder.ClientEndpoint + " - response " + this.response);
+            responder.Respond(this.webResponse);
+        }
+
+        private string webResponse
+        {
+            get
+            {
+                return "{\"DeviceId\":\"" +
+                    hgd.IdentifierString + "\","
+                    + "\"distance\":" + this.distance_US3.GetDistanceInCentimeters().ToString() +
+                    "}";
+            }
+        }
+
+        private string response
+        {
+            get
+            {
+                return "{" +
+                "\"distance\" : " + this.distance_US3.GetDistanceInCentimeters().ToString() + "\n" +
+                 "\"DeviceIP\" : \"" + this.wifi.NetworkSettings.IPAddress + "\", " +
+                 "\"DeviceId\" : \"" + hgd.IdentifierString + "\", " +
+                "}";
+            }
+        }
+    }
+}
